e6b193a6772c5dfd4044e2161220c8818e849117
Fra <135766845+JungleKiosk@users.noreply.github.com>
2023-09-22 23:33:40 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">Random Number</h1>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
