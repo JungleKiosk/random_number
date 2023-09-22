3956b3d2257451374870b2becf2a3c7a9dc28c5b
Fra <135766845+JungleKiosk@users.noreply.github.com>
2023-09-23 00:07:04 +0200

    export default

--- src/components/RandoNumber.vue
+++ src/components/RandoNumber.vue
@@ -1,33 +1,46 @@
+<script>
+export default {
+    data() {
+        return {
+            randomNumber: null,
+            imageVisible: false
+        };
+    },
+    methods: {
+        generateRandomNumber() {
+            let randomNum;
+            do {
+                randomNum = Math.floor(Math.random() * 21) + 1;
+            } while (randomNum === 7 || randomNum === 8);
 
-<script setup>
-import { ref } from 'vue';
-
-const numbers = [...Array(22).keys()].filter(num => num !== 7 && num !== 8 && num !== 22);
-const randomNumber = ref(null);
-
-const generateRandomNumber = () => {
-    const randomIndex = Math.floor(Math.random() * numbers.length);
-    randomNumber.value = numbers[randomIndex];
-};
-
-const resetRandomNumber = () => {
-    randomNumber.value = null;
-};
-
+            this.randomNumber = randomNum;
+            this.imageVisible = true; // Mostra l'immagine
+        },
+        resetRandomNumber() {
+            this.randomNumber = null;
+            this.imageVisible = false; // Nascondi l'immagine
+        }
+    }
+}
 </script>
 
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-6">
                 <h1>Choose</h1>
-                <p>Random Number: {{ randomNumber }}</p>
+                <p v-if="randomNumber !== null">Random Number: {{ randomNumber }}</p>
+                <p v-else>Click 'Generate Random Number' to see the number</p>
                 <button @click="generateRandomNumber">Generate Random Number</button>
                 <button @click="resetRandomNumber">Reset</button>
+            </div>
+            <div class="col-6">
+                <img v-if="imageVisible" src="path_to_your_image.jpg" alt="Random Image" />
             </div>
         </div>
     </div>
 </template>
-  
-<style scoped></style>
-  
+
+<style scoped>
+/* Stili CSS opzionali */
+</style>
